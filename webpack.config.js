7d3d50f4927be163b763a5a1363dca842e2b0c62
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-07-06 10:48:05 +1000

    Include babel transpiler in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-     title: "Caching",
-     template: "./src/template.html",
+      title: 'Caching',
+      template: './src/template.html',
     }),
   ],
   devtool: 'inline-source-map',
@@ -18,6 +17,9 @@ module.exports = {
     filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    environment: {
+      arrowFunction: false,
+    },
   },
   module: {
     rules: [
@@ -25,13 +27,25 @@ module.exports = {
         test: /\.css$/i,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-     ],
-   },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
